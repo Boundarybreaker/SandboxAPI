0403dec9400d2314bf8f3fe0a3278bd7a75daa35
Meredith Espinosa <goclonefilms@gmail.com>
2019-11-30 02:13:18 -0800

    oops the other reindent too

--- src/main/java/org/sandboxpowered/sandbox/api/component/fluid/FluidLoggingContainer.java
+++ src/main/java/org/sandboxpowered/sandbox/api/component/fluid/FluidLoggingContainer.java
@@ -13,60 +13,60 @@ import org.sandboxpowered.sandbox.api.world.WorldReader;
 import java.util.function.Predicate;
 
 public class FluidLoggingContainer implements FluidContainer {
-	private FluidLoggable loggable;
-	private WorldReader world;
-	private Position pos;
-	private BlockState state;
-	private Mono<Direction> direction;
+    private FluidLoggable loggable;
+    private WorldReader world;
+    private Position pos;
+    private BlockState state;
+    private Mono<Direction> direction;
 
-	public FluidLoggingContainer(FluidLoggable loggable, WorldReader world, Position pos, BlockState state, Mono<Direction> direction) {
-		this.loggable = loggable;
-		this.world = world;
-		this.pos = pos;
-		this.state = state;
-		this.direction = direction;
-	}
+    public FluidLoggingContainer(FluidLoggable loggable, WorldReader world, Position pos, BlockState state, Mono<Direction> direction) {
+        this.loggable = loggable;
+        this.world = world;
+        this.pos = pos;
+        this.state = state;
+        this.direction = direction;
+    }
 
-	@Override
-	public boolean isEmpty() {
-		return loggable.getFluid(world, pos, state, direction).isEmpty();
-	}
+    @Override
+    public boolean isEmpty() {
+        return loggable.getFluid(world, pos, state, direction).isEmpty();
+    }
 
-	@Override
-	public int getSize() {
-		return 1;
-	}
+    @Override
+    public int getSize() {
+        return 1;
+    }
 
-	@Override
-	public int getMaxStackSize(int slot) {
-		return 1000;
-	}
+    @Override
+    public int getMaxStackSize(int slot) {
+        return 1000;
+    }
 
-	@Override
-	public FluidStack get(int slot) {
-		return loggable.getFluid(world, pos, state, direction);
-	}
+    @Override
+    public FluidStack get(int slot) {
+        return loggable.getFluid(world, pos, state, direction);
+    }
 
-	@Override
-	public FluidStack extract(int slot, Predicate<FluidStack> fluidFilter, int amount, boolean simulate) {
-		if (fluidFilter.test(get(0))) {
-			return loggable.drainFrom(world, pos, state, amount, direction,  simulate);
-		}
-		return FluidStack.empty();
-	}
+    @Override
+    public FluidStack extract(int slot, Predicate<FluidStack> fluidFilter, int amount, boolean simulate) {
+        if (fluidFilter.test(get(0))) {
+            return loggable.drainFrom(world, pos, state, amount, direction,  simulate);
+        }
+        return FluidStack.empty();
+    }
 
-	@Override
-	public FluidStack insert(int slot, FluidStack stack, boolean simulate) {
-		if (loggable.canContainFluid(world, pos, state, stack.getFluid(), direction) && stack.getVolume() >= 1000) {
-			return loggable.fillWith(world, pos, state, stack, direction, simulate);
-		}
-		return stack;
-	}
+    @Override
+    public FluidStack insert(int slot, FluidStack stack, boolean simulate) {
+        if (loggable.canContainFluid(world, pos, state, stack.getFluid(), direction) && stack.getVolume() >= 1000) {
+            return loggable.fillWith(world, pos, state, stack, direction, simulate);
+        }
+        return stack;
+    }
 
-	@Override
-	public void setStack(int slot, FluidStack stack) {
-		if (loggable.canContainFluid(world, pos, state, stack.getFluid(), direction)) {
-			loggable.fillWith(world, pos, state, stack, direction, true);
-		}
-	}
+    @Override
+    public void setStack(int slot, FluidStack stack) {
+        if (loggable.canContainFluid(world, pos, state, stack.getFluid(), direction)) {
+            loggable.fillWith(world, pos, state, stack, direction, true);
+        }
+    }
 }
